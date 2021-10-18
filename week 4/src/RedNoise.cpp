ed577382c29e69ef886bacd06231a6deb4a2ce83
lokhei <lokhei01@gmail.com>
2021-10-18 18:03:16 +0100

    week 4 exercies 8

--- week 4/src/RedNoise.cpp
+++ week 4/src/RedNoise.cpp
@@ -3,38 +3,104 @@
 #include <Utils.h>
 #include <fstream>
 #include <vector>
+#include <glm/glm.hpp>
+#include <CanvasPoint.h>
+#include <CanvasTriangle.h>
+#include <Colour.h>
 #include <ModelTriangle.h>
+#include <TextureMap.h>
+#include <TexturePoint.h>
+#include <Utils.h>
+#include <unordered_map>
 
 #define WIDTH 320
 #define HEIGHT 240
 
 
-std::vector<Colour> loadMtlFile(const std::string &filename) {
-	std::vector<Colour> colours;
+std :: vector<float> interpolateSingleFloats(float from, float to, int numberOfValues) {
+	float step = (to - from) / (numberOfValues -1);
+	std :: vector<float> v;
+	for (int i = 0; i < numberOfValues; i++){
+		v.push_back(from + i*step);
+	}
+	return v;
+}
+
+template <typename T>
+std::vector<CanvasPoint> interpolateRoundPoints(T from, T to, int numberOfValues) {
+	std::vector<CanvasPoint> points;
+	std :: vector<float> xs = interpolateSingleFloats(from.x, to.x, numberOfValues);
+	std :: vector<float> ys = interpolateSingleFloats(from.y, to.y, numberOfValues);
+	for (int i=0; i<numberOfValues; i++) {
+		points.push_back(CanvasPoint(round(xs[i]), round(ys[i])));
+	}
+	return points;
+}
+
+void sortTriangle(CanvasTriangle &triangle){
+	if (triangle[0].y > triangle[1].y) {
+		std::swap(triangle[0], triangle[1]);
+	}
+	if (triangle[1].y > triangle[2].y) {
+		std::swap(triangle[1], triangle[2]);
+		if (triangle[0].y > triangle[1].y) {
+			std::swap(triangle[0], triangle[1]);
+		}	
+	}
+}
+
+void drawLine(DrawingWindow &window, CanvasPoint from, CanvasPoint to, Colour colour) {
+	float numberOfSteps = std::max(abs(to.x - from.x), abs(to.y - from.y));
+	std :: vector<CanvasPoint> points = interpolateRoundPoints(from, to, numberOfSteps + 1);
+	for (int i=0; i<=numberOfSteps; i++) {
+		window.setPixelColour(points[i].x, points[i].y, (255 << 24) + (colour.red << 16) + (colour.green << 8) + colour.blue);
+	}
+}
+
+void drawStrokedTriangle(DrawingWindow &window, CanvasTriangle triangle, Colour colour) {
+	drawLine(window, triangle[0], triangle[1], colour);
+	drawLine(window, triangle[0], triangle[2], colour);
+	drawLine(window, triangle[1], triangle[2], colour);
+}
+
+void drawFilledTriangle(DrawingWindow &window, CanvasTriangle triangle, Colour colour, bool outline) {
+	sortTriangle(triangle);
+	std :: vector<CanvasPoint> start = interpolateRoundPoints(triangle[0], triangle[1], triangle[1].y - triangle[0].y + 1);
+	start.pop_back(); // Last row duplicated by next triangle so pop
+	std :: vector<CanvasPoint> start2 = interpolateRoundPoints(triangle[1], triangle[2], triangle[2].y - triangle[1].y + 1);
+	start.insert(start.end(), start2.begin(), start2.end());
+	std :: vector<CanvasPoint> end = interpolateRoundPoints(triangle[0], triangle[2], triangle[2].y - triangle[0].y + 1);
+
+	// Draw the filled in triangle
+	for (int i=0; i<=triangle[2].y - triangle[0].y; i++) {
+		drawLine(window, start[i], end[i], colour);
+	}
+
+	if (outline) drawStrokedTriangle(window, triangle, Colour(255,255,255)); //outline
+
+}
+std::unordered_map<std::string, Colour> loadMtlFile(const std::string &filename) {
+	std::unordered_map<std::string, Colour> colours;
 
 	std::ifstream inputStream(filename, std::ifstream::in);
 	std::string nextLine;
-	std::string name;
+	std::string colour_name;
 
 	while (std::getline(inputStream, nextLine)) {
 		std::vector<std::string> line = split(nextLine, ' ');
 
 		if (line[0] == "newmtl") {
-			name = line[1];
+			colour_name = line[1];
 		} else if (line[0] == "Kd") {
-			colours.push_back(Colour(
-				name,
-				(int)(std::stof(line[1]) * 255),
-				(int)(std::stof(line[2]) * 255),
-				(int)(std::stof(line[3]) * 255)
-			));
+			Colour colour(int(std::stof(line[1])*255),int(std::stof(line[2])*255),int(std::stof(line[3])*255));
+			colours.insert({colour_name, colour});
 		}
 	}
+	inputStream.close();
 	return colours;
 }
 
-
-std::vector<ModelTriangle> loadObjFile(const std::string &filename, float scale, std::vector<Colour> materials) {
+std::vector<ModelTriangle> loadObjFile(const std::string &filename, float scale, std::unordered_map<std::string, Colour> materials) {
 	std::vector<glm::vec3> vertices;
 	std::vector<ModelTriangle> faces;
 
@@ -43,14 +109,10 @@ std::vector<ModelTriangle> loadObjFile(const std::string &filename, float scale,
 	Colour colour;
 	while (std::getline(inputStr, nextLine)) { //extracts from inputStr and stores into nextLine
 		std::vector<std::string> vector = split(nextLine, ' '); //split line by spaces
-		
+
+
 		if (vector[0] == "usemtl") {
-			for (int i=0; i<materials.size(); i++) {
-				if (materials[i].name == vector[1]) {
-					colour = materials[i];
-					break;
-				}
-			}
+			colour = materials[vector[1]];
 		}else if (vector[0] == "v") {
 			vertices.push_back(glm::vec3(
 				std::stof(vector[1]) * scale, //string to float
@@ -67,21 +129,36 @@ std::vector<ModelTriangle> loadObjFile(const std::string &filename, float scale,
 			));
 		}
 	}
+
+	inputStr.close();
 	return faces;
 }
 
 
-void draw(DrawingWindow &window) {
+
+
+CanvasPoint getCanvasIntersectionPoint(DrawingWindow &window, glm::vec3 cameraPosition,glm::vec3 vertexPosition, float focalLength){
+
+	int planeMultiplier = 100;
+	glm::vec3 vertex = vertexPosition - cameraPosition;
+	float u = round(-planeMultiplier*focalLength * (vertex.x / vertex.z)) + (window.width / 2);
+	float v = round(planeMultiplier*focalLength * (vertex.y / vertex.z)) + (window.height / 2);
+
+	return CanvasPoint(u,v);
+}
+
+void pointcloud(DrawingWindow &window, glm::vec3 cameraPosition, std::vector<ModelTriangle> faces, float focalLength) {
 	window.clearPixels();
-	for (size_t y = 0; y < window.height; y++) {
-		for (size_t x = 0; x < window.width; x++) {
-			float red = rand() % 256;
-			float green = 0.0;
-			float blue = 0.0;
-			uint32_t colour = (255 << 24) + (int(red) << 16) + (int(green) << 8) + int(blue);
-			window.setPixelColour(x, y, colour);
+
+	for (int i=0; i<faces.size(); i++) {
+		CanvasTriangle triangle = CanvasTriangle();
+		for (int j=0; j<faces[i].vertices.size(); j++) {
+			glm::vec3 vertexPosition = faces[i].vertices[j];
+			triangle.vertices[j] = getCanvasIntersectionPoint(window,cameraPosition, vertexPosition, focalLength );
 		}
+		drawFilledTriangle(window, triangle, faces[i].colour, false);
 	}
+	
 }
 
 void handleEvent(SDL_Event event, DrawingWindow &window) {
@@ -99,20 +176,18 @@ void handleEvent(SDL_Event event, DrawingWindow &window) {
 int main(int argc, char *argv[]) {
 	DrawingWindow window = DrawingWindow(WIDTH, HEIGHT, false);
 	SDL_Event event;
+	
+	float vertexScale = 0.5;
 
-	float vertexScale = 0.17;
+	std::unordered_map<std::string, Colour> materials = loadMtlFile("cornell-box.mtl");
 
-	std::vector<Colour> materials = loadMtlFile("cornell-box.mtl");
 	std::vector<ModelTriangle> faces = loadObjFile("cornell-box.obj", vertexScale, materials);
-	for (int i=0; i<faces.size(); i++) {
-		std::cout << faces[i] << std::endl;
-	}
+	glm::vec3 cameraPosition = glm::vec3(0.0, 0.0, 4.0);
+	float focalLength = 2.0;
 
 	while (true) {
-		// We MUST poll for events - otherwise the window will freeze !
 		if (window.pollForInputEvents(event)) handleEvent(event, window);
-		draw(window);
-		// Need to render the frame at the end, or nothing actually gets shown on the screen !
+		pointcloud(window, cameraPosition, faces, focalLength);
 		window.renderFrame();
 	}
 }
